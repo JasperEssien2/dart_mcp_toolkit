e5f619620b7492929fe83a8f014df61c24713cc2
Jahswill Essien <jasperessien2@gmail.com>
2025-11-08 21:30:50 +0100

    moved CallablePropertySchema to its own file out of model_tool_mapper.dart

--- lib/src/model_tool_mapper.dart
+++ lib/src/model_tool_mapper.dart
@@ -3,6 +3,7 @@ import 'dart:mirrors';
 import 'package:collection/collection.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mcp_toolkit/src/annotations/annotations.dart';
+import 'package:mcp_toolkit/src/models/callable_propery_schema.dart';
 
 class ModelToolMapper {
   ModelToolMapper({required this.toolInput});
@@ -168,76 +169,3 @@ class CallableTool extends Equatable {
   List<Object> get props => [toolName, toolDescription, properties];
 }
 
-sealed class CallablePropertySchema extends Equatable {
-  const CallablePropertySchema({required this.name, required this.description, required this.isRequired});
-
-  final String name;
-  final String description;
-  final bool isRequired;
-
-  @override
-  List<Object?> get props => [name, description, isRequired];
-}
-
-class StringSchema extends CallablePropertySchema {
-  const StringSchema({required super.name, required super.description, required super.isRequired});
-}
-
-class BooleanSchema extends CallablePropertySchema {
-  const BooleanSchema({required super.name, required super.description, required super.isRequired});
-}
-
-class NumberSchema extends CallablePropertySchema {
-  const NumberSchema({required super.name, required super.description, required super.isRequired});
-}
-
-class IntSchema extends CallablePropertySchema {
-  const IntSchema({required super.name, required super.description, required super.isRequired});
-}
-
-class ListSchema extends CallablePropertySchema {
-  const ListSchema({required super.name, required super.description, required super.isRequired, required this.type});
-
-  final ListType type;
-
-  @override
-  List<Object?> get props => [...super.props, type];
-}
-
-class EnumSchema extends CallablePropertySchema {
-  const EnumSchema({required super.name, required super.description, required super.isRequired, required this.options});
-
-  final List<String> options;
-
-  @override
-  List<Object?> get props => [...super.props, options];
-}
-
-class ObjectSchema extends CallablePropertySchema {
-  const ObjectSchema({
-    required super.name,
-    required super.description,
-    required super.isRequired,
-    required this.properties,
-  });
-
-  final List<CallablePropertySchema> properties;
-
-  @override
-  List<Object?> get props => [...super.props, properties];
-}
-
-class NullSchema extends CallablePropertySchema {
-  const NullSchema({required super.name, required super.description, required super.isRequired});
-}
-
-class InvalidSchema extends CallablePropertySchema {
-  const InvalidSchema({required super.name, required super.description, super.isRequired = false, required this.error});
-
-  final String error;
-
-  @override
-  List<Object?> get props => [...super.props, error];
-}
-
-enum ListType { int, num, string, boolean, enumerated, object, unknown }
